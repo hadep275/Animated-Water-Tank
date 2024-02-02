cdf84a224552c1e5352a44d740caa6209aabd8ce
hadep275 <65734173+hadep275@users.noreply.github.com>
2024-02-01 18:26:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-<script>
-      const input = document.querySelector("input");
-      const indicator = document.querySelector(".indicator");
-      const output = document.querySelector("output");
-      input.addEventListener("input", (e) => {
-        setValue(e.target.value);
-      });
-      function setValue(val) {
-        output.value = val;
-        indicator.setAttribute("style", `--completion: ${val}%`);
-      }
-    </script>
+const input = document.querySelector("input");
+const indicator = document.querySelector(".indicator");
+const output = document.querySelector("output");
+input.addEventListener("input", (e) => {
+      setValue(e.target.value);
+});
+function setValue(val) {
+      output.value = val;
+      indicator.setAttribute("style", `--completion: ${val}%`);
+}
