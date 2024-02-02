56ce2d8c462be5cf033685fb6b0371f4396a79b7
hadep275 <65734173+hadep275@users.noreply.github.com>
2024-02-01 18:14:09 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+      const input = document.querySelector("input");
+      const indicator = document.querySelector(".indicator");
+      const output = document.querySelector("output");
+      input.addEventListener("input", (e) => {
+        setValue(e.target.value);
+      });
+      function setValue(val) {
+        output.value = val;
+        indicator.setAttribute("style", `--completion: ${val}%`);
+      }
+    </script>
